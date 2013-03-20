3ce8de6e987de98760623bf58e73d9d6a48bd44a
尹常鑫 <ycxxww@gmail.com>
2013-03-20 17:21:16 +0800

    完善第一个版本

--- app/controllers/feedback/feedback_infos_controller.rb
+++ app/controllers/feedback/feedback_infos_controller.rb
@@ -1,10 +1,27 @@
 module Feedback
   class FeedbackInfosController < ApplicationController
+    respond_to :html, :json, :js
+
     def index
+      @feedback_infos = FeedbackInfo.paginate(:page => params[:page], :per_page => 30)
+    end
+
+    def new
+      @feedback_info = FeedbackInfo.new
+      render :layout => false
+    end
+
+    def show
+      @feedback_info = FeedbackInfo.find(params[:id]) 
     end
 
     def create
-      
+      @feedback_info = FeedbackInfo.new(params[:feedback_info])
+      if @feedback_info.save then
+        redirect_to feedback_infos_path 
+      else
+        redirect_to root_path
+      end
     end
   end
 end
