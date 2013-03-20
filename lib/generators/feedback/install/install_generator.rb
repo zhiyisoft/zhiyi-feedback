5c1922955e840001aaf1cecfefd0ad853d18d083
尹常鑫 <ycxxww@gmail.com>
2013-03-20 17:59:00 +0800

    修改安装的一个Bug

--- lib/generators/feedback/install/install_generator.rb
+++ lib/generators/feedback/install/install_generator.rb
@@ -8,9 +8,4 @@ class Feedback::InstallGenerator < Rails::Generators::Base
     route %Q(mount Feedback::Engine => '/feedback', as: 'feedback_app')
   end
 
-  def copy_additional_file
-    copy_file 'tree.jquery.js', 'app/assets/javascripts/tree.jquery.js'
-    copy_file 'jqtree.css', 'app/assets/stylesheets/jqtree.css'
-  end
-
 end
