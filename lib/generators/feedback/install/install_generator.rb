b2a52dddaa522afc636dfd7f7ada72ec814dae8c
尹常鑫 <ycxxww@gmail.com>
2013-03-21 23:58:08 +0800

    添加邮件发送功能

--- lib/generators/feedback/install/install_generator.rb
+++ lib/generators/feedback/install/install_generator.rb
@@ -1,11 +1,17 @@
 class Feedback::InstallGenerator < Rails::Generators::Base
   source_root File.expand_path('../templates', __FILE__)
-  
+
   def inject_gem_dependeny
     gem 'slim'
     gem 'simple_form'
+    gem 'settingslogic'
 
     route %Q(mount Feedback::Engine => '/feedback', as: 'feedback_app')
+  end
+
+  def copy_additional_file
+    copy_file 'setting.rb', 'app/models/setting.rb'
+    copy_file 'settings.yml', 'config/settings.yml'
   end
 
 end
