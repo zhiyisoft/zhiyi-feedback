7e745c8250d4d724e6459df8b69f0233b66b9bd3
尹常鑫 <ycxxww@gmail.com>
2013-05-08 21:06:16 +0800

    提交成功后业务逻辑

--- app/views/feedback/feedback_infos/create.js
+++ app/views/feedback/feedback_infos/create.js
@@ -1,2 +1,7 @@
-$('#myModal').modal('hide');
-alert("提交成功！");
+<% if @flag %>
+  $('#myModal').modal('hide');
+  alert("提交成功！");
+<% else %>
+  $(".alert").show();
+  $(".alert").html('<p>提交失败，请填写完整重试！</p>');
+<% end %>
